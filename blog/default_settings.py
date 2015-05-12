420c6e7c1cc5505f7465f7a7ec4c6ad2f4ea7748
chriskiehl <audionautic@gmail.com>
2015-05-11 20:46:54 -0400

    Removed middleware test

--- blog/default_settings.py
+++ blog/default_settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'main.middleware.ConditionalSessionMiddleware',
+    # 'main.middleware.ConditionalSessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
